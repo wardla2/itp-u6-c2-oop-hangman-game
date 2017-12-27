cef1bc0204ae24059923c7f59ec13216c9231e18
Laura <wardla2@gmail.com>
2017-12-26 17:25:48 -0700

    first try outline

--- hangman/game.py
+++ hangman/game.py
@@ -1,13 +1,29 @@
 from .exceptions import *
 
-
+#GuessAttempt: An attempt to guess a letter.
 class GuessAttempt(object):
     pass
+    
+    def __init__(self):
+        pass
+    
+    def is_hit():
+        '''new_masked = ''
+           for i in range(len(answer_word)):
+                if answer_word[i].lower() == character.lower():
+                    new_masked += answer_word[i].lower()
+                else:
+                    new_masked += masked_word[i]
+        return new_masked''' 
+        pass
+    
+    def is_miss():
+        pass
 
-
-class GuessWord(object):
+#GuessWord: A word to guess. Is used by a HangmanGame to keep track of the word to guess.
+class GuessWord(object): 
     pass
 
-
+#HangmanGame: the main interface for the user, the "general" game that will be used.
 class HangmanGame(object):
     pass
